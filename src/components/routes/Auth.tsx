dc5dcf1e09ecd49bdc17808bef2dcff3990fe46f
정규영 <jgy_98@naver.com>
2023-04-26 18:27:01 +0900

    230426-user login

--- src/components/routes/Auth.tsx
+++ src/components/routes/Auth.tsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+import React, { useState } from 'react';
 
 const Auth =  () => {
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: '',
+    });
+    const [newAccount, setNewAccount] = useState(true);
+    const { email, password } = inputs;
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    }
+    const onSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            let data;
+            const auth = getAuth();
+            if(newAccount) {
+                // create account (새로운 계산 생성)
+                data = createUserWithEmailAndPassword(auth, email, password)
+            }else {
+                // login
+                data = await signInWithEmailAndPassword(auth, email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
-        <>Auth</>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text" 
+                    placeholder='Email' 
+                    required value={email} 
+                    name='email'
+                    onChange={onChange}
+                />
+                <input 
+                    type='password' 
+                    placeholder="Password" 
+                    required value={password} 
+                    name='password'
+                    onChange={onChange}
+                />
+                <input type='submit' value={newAccount ? "Create Account" : "Login"} />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+            </div>
+        </div>
     );
 };
 
